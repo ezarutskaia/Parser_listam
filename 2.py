471af2099773540148cfab22d48435494769f99b
Elena <ezarutskaia@gmail.com>
2023-06-07 15:05:56 +0400

    Цена без $

--- 2.py
+++ 2.py
@@ -1,5 +1,12 @@
 import pandas as pd
+import re
 
 df = pd.read_csv('df.csv', delimiter=',')
+
+search1 = lambda s: s.lstrip("$")
+df['price'] = df['price'].map(search1)
+
+search2 = lambda x: x.split(',')
+df['car'] = df['model'].map(search2)
 
 print(df.head(5))
