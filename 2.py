0523f2dca3b7493566b6ea3e200f6bf50834f865
Elena <ezarutskaia@gmail.com>
2023-06-08 14:53:11 +0400

    Подготовка данных 08.06

--- 2.py
+++ 2.py
@@ -3,13 +3,16 @@ import re
 
 df = pd.read_csv('df.csv', delimiter=',')
 
-search = lambda s: s.lstrip("$")
-df['price'] = df['price'].map(search)
+search1 = lambda s: s.lstrip("$")
+df['price'] = df['price'].map(search1)
 
 expanded = df['model'].str.split(pat=',', n=3, expand=True)
 df['car'] = expanded[0]
 df['eng_cap'] = expanded[1]
 df['year'] = expanded[2]
+
+expanded0 = df['car'].str.split(pat=' ', expand=True)
+df['brand'] = expanded0[0]
 
 expanded1 = df['mileage'].str.split(pat=',', n=5, expand=True)
 df['1000*km'] = expanded1[2]
@@ -24,6 +27,17 @@ for ind in df.index:
         df['1000*km'][ind] = int(int(df['1000*km'][ind]) * 1.6)
         print(type(df['1000*km'][ind]))
 
+search2 = lambda x: x.rstrip(" л.")
+df['eng_cap'] = df['eng_cap'].map(search2)
+search3 = lambda x: x.rstrip(" г.")
+df['year'] = df['year'].map(search3)
+
 df.drop(['model', 'mileage', 'si', 'unit'], axis= 1 , inplace= True )
 
-print(df.head(10))
+
+search4 = lambda x: x if re.search(r"\d{4}", x) else 0
+df['year'] = df['year'].map(search4)
+
+
+
+print(df.head(50))
